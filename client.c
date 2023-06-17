b8e0ae5a4959628dc2bdb6828e4a6fc99797a3f0
Antoine Wyplosz <awyplosz@gmail.com>
2023-06-17 16:17:35 +0200

    english version

--- client.c
+++ client.c
@@ -13,52 +13,56 @@ int main() {
     struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE] = {0};
 
+    // Create a socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("Erreur lors de la création du socket\n");
+        printf("Error creating socket\n");
         return -1;
     }
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
+    // Convert IP address from text to binary form
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("Adresse invalide / Adresse non supportée\n");
+        printf("Invalid address/Address not supported\n");
         return -1;
     }
 
+    // Connect to the server
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("Connexion échouée\n");
+        printf("Connection failed\n");
         return -1;
     }
 
-    // Lire les entrées de l'utilisateur et envoyer les données au serveur
+    // Read user inputs and send data to the server
     char category[BUFFER_SIZE];
-    printf("Catégorie de la place (gradin 1, gradin 2, fosse) : ");
+    printf("Category of the seat (grandstand 1, grandstand 2, standing) : ");
     fgets(category, sizeof(category), stdin);
     category[strcspn(category, "\n")] = '\0';
     send(sock, category, strlen(category), 0);
 
     char numPlaces[BUFFER_SIZE];
-    printf("Nombre de places : ");
+    printf("Number of seats: ");
     fgets(numPlaces, sizeof(numPlaces), stdin);
     numPlaces[strcspn(numPlaces, "\n")] = '\0';
     send(sock, numPlaces, strlen(numPlaces), 0);
 
     char name[BUFFER_SIZE];
-    printf("Nom et prénom : ");
+    printf("Name and surname: ");
     fgets(name, sizeof(name), stdin);
     name[strcspn(name, "\n")] = '\0';
     send(sock, name, strlen(name), 0);
 
-    // Attendre la réponse du serveur et l'afficher
+    // Wait for the server's response and display it
     memset(buffer, 0, sizeof(buffer));
     int bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
     if (bytes_received > 0) {
-        printf("Réponse du serveur : %s\n", buffer);
+        printf("Server's response: %s\n", buffer);
     } else {
-        printf("Erreur lors de la réception de la réponse du serveur\n");
+        printf("Error receiving server's response\n");
     }
 
+    // Close the socket
     close(sock);
     return 0;
 }
