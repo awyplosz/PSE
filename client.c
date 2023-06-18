363c69ae7df37731e9fe84f5dafde131286b99f8
Antoine Wyplosz <awyplosz@gmail.com>
2023-06-18 17:19:29 +0200

    treasury

--- client.c
+++ client.c
@@ -5,8 +5,11 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+
 #define PORT 2000
 #define BUFFER_SIZE 1024
+
+void server_response(int sock);
 
 int main() {
     int sock = 0;
@@ -38,43 +41,60 @@ int main() {
 
     // Read user inputs and send data to the server
     char category[BUFFER_SIZE];
-    printf("Category of the seat (grandstand 1, grandstand 2, standing): ");
+    printf("Welcome to the concert ticket sales server !\n");
+    printf("There are 3 categories of seat you can buy :\n");
+    printf("grandstand 1 : 100 euros\ngrandstand 2 : 50 euros \nstanding : 80 euros\n"); 
+    printf("You can buy several tickets in the same category\n"); 
+    printf("Which category do you want to buy : ");
     fgets(category, sizeof(category), stdin);
     category[strcspn(category, "\n")] = '\0';
     send(sock, category, strlen(category) + 1, 0);
 
+    server_response(sock);
+
     char numPlaces[BUFFER_SIZE];
-    printf("Number of seats: ");
+    printf("Choose the number of seats : ");
     fgets(numPlaces, sizeof(numPlaces), stdin);
     numPlaces[strcspn(numPlaces, "\n")] = '\0';
     send(sock, numPlaces, strlen(numPlaces) + 1, 0);
 
+    server_response(sock);
 
     char name[BUFFER_SIZE];
     printf("Name and surname: ");
     fgets(name, sizeof(name), stdin);
     name[strcspn(name, "\n")] = '\0';
     send(sock, name, strlen(name) + 1, 0);
+    
 
+    server_response(sock);
+    
+    char treasury[BUFFER_SIZE];
+    fgets(treasury, sizeof(treasury), stdin);
+    treasury[strcspn(treasury, "\n")] = '\0';
+    send(sock, treasury, strlen(treasury) + 1, 0);
 
-   // Receive and display the server's response
-    while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        int bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
-        if (bytes_received > 0) {
-            printf("Server's response: %s\n", buffer);
-        } else if (bytes_received == 0) {
-            printf("Connection closed by the server\n");
-            break;
-        } else {
-            printf("Error receiving server's response\n");
-            break;
-        }
-    }
-
+    server_response(sock);
 
 
     // Close the socket
     close(sock);
     return 0;
+}
+
+void server_response(int sock){
+
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, sizeof(buffer));
+    int bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    if (bytes_received > 0) {
+        printf("Server's response: %s\n", buffer);
+    } else if (bytes_received == 0) {
+        printf("Connection closed by the server\n");
+        return;
+    } else {
+        printf("Error receiving server's response\n");
+        return;
+    }
+
 }
